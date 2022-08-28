fa4ca6cb40f02d8a8b3592d394b01a095fbeff34
kevin91nl <kevin91nl@gmail.com>
2022-08-28 21:12:58 +0200

    Implementation of dataset classes and addition of a few data files

--- src/dataset.py
+++ src/dataset.py
@@ -1,1 +1,73 @@
 """Dataset loaders."""
+from dataclasses import dataclass
+from omegaconf import DictConfig
+from torch.utils.data import Dataset
+import pandas as pd
+from typing import List
+from enum import Enum
+
+
+class ProvisionType(Enum):
+    """The provision type."""
+
+    permission = "permission"
+    prohibition = "prohibition"
+    duty = "duty"
+
+
+@dataclass
+class ProvisionFrame:
+    """The frame consisting of a provision type (text classification) and entities (named entity recognition)."""
+
+    provision_type: str
+    bearer: str
+    action: str
+    other_party: str
+
+
+@dataclass
+class DatasetItem:
+    """A dataset item, consisting of the context and zero or more provisions."""
+
+    context: str
+    provisions: List[ProvisionFrame]
+
+
+class ConfiguredDataset(Dataset):  # type: ignore
+    """A configured dataset."""
+
+    def __init__(self, config: DictConfig) -> None:
+        """Initialize the dataset.
+
+        Parameters
+        ----------
+        config : DictConfig
+            The configuration of the dataset.
+        """
+        super().__init__()
+        self.data = pd.read_json(config.path, orient="records", lines=True)
+
+    def __getitem__(self, index: int) -> DatasetItem:
+        """Get the item at the given index.
+
+        Parameters
+        ----------
+        index : int
+            The index.
+
+        Returns
+        -------
+        DatasetItem
+            The item.
+        """
+        return DatasetItem(**self.data.iloc[index % len(self)].to_dict())
+
+    def __len__(self) -> int:
+        """Get the length of the dataset.
+
+        Returns
+        -------
+        int
+            The length of the dataset.
+        """
+        return len(self.data)
--- src/running.py
+++ src/running.py
@@ -1,13 +1,22 @@
 """A class for running the code."""
 
+from dataset import ConfiguredDataset
 from pipeline.information_extraction_models import InformationExtractionModel
 from pipeline.tokenizer_models import Tokenizer
+from typing import Optional
 
 
 class Runner:
     """A class for running the code."""
 
-    def __init__(self, tokenizer: Tokenizer, model: InformationExtractionModel) -> None:
+    def __init__(
+        self,
+        tokenizer: Tokenizer,
+        model: InformationExtractionModel,
+        train_dataset: Optional[ConfiguredDataset] = None,
+        test_dataset: Optional[ConfiguredDataset] = None,
+        validation_dataset: Optional[ConfiguredDataset] = None,
+    ) -> None:
         """Initialize the runner.
 
         Parameters
@@ -16,10 +25,19 @@ class Runner:
             The tokenizer.
         model : InformationExtractionModel
             The model.
+        train_dataset : Optional[ConfiguredDataset]
+            The train dataset.
+        test_dataset : Optional[ConfiguredDataset]
+            The test dataset.
+        validation_dataset : Optional[ConfiguredDataset]
+            The validation dataset.
         """
         super().__init__()
         self._tokenizer = tokenizer
         self._model = model
+        self._train_dataset = train_dataset
+        self._test_dataset = test_dataset
+        self._validation_dataset = validation_dataset
 
     def run(self) -> None:
         """Run the code."""
